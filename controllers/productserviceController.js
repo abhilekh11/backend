deead659a49d396be711b49ad22a67c69e6fca33
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-10-19 18:37:15 +0530

    ss

---
+++ controllers/productserviceController.js
@@ -0,0 +1,7 @@
+
+ const Product_Service=require('../models/productserviceModel');
+
+ const catchAsyncErrors=require('../middleware/catchAsyncErrors');
+ const ErrorHander = require("../utils/errorhander");
+
+//    exports.Product_Service=catchAsyncErrors(async())
