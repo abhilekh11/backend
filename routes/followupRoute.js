c0133683076839d067e2a8f17096761843013781
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-11-08 16:46:11 +0530

    ss

--- routes/followupRoute.js
+++ routes/followupRoute.js
@@ -4,6 +4,6 @@ const { Add_Followup_Lead, getFollowupById } = require('../controllers/followupC
 
 const router=express.Router();
 router.route("/add_followup_lead").post(Add_Followup_Lead);
-router.route("/all_followup_lead_by_id/:id").post(getFollowupById);
+router.route("/all_followup_lead_by_id/:id").get(getFollowupById);
 
 module.exports=router;     
