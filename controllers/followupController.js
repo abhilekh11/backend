20a2ac53cd381840bf4d0a4e5c5e066795c194d7
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-11-08 16:03:30 +0530

    ss

--- controllers/followupController.js
+++ controllers/followupController.js
@@ -4,6 +4,7 @@ const FollowupLead=require('../models/followupModel');
 
 const catchAsyncErrors=require('../middleware/catchAsyncErrors');
 const ErrorHander = require("../utils/errorhander");
+const { param } = require('../app');
 
 
 /// creat followup Lead
@@ -17,7 +18,58 @@ exports.Add_Followup_Lead=catchAsyncErrors(async (req,res,next)=>{
 })
 
 
-/// 
+/// get follow up lead by lead id 
+
+exports.getFollowupById=catchAsyncErrors(async(req,res,next)=>{
+        
+       const followuplead1=await FollowupLead.find({lead_id:req.params.id}); 
+       
+       if(!followuplead1){
+        return next(new ErrorHander("followuplead not found!...",404));
+
+        
+       }else{
+        const followuplead = await FollowupLead.aggregate([ 
+          {
+            $match: {
+              $expr: {
+                $eq: ["$lead_id", req.params.id ],
+              },
+            },
+          },
+      
+          {
+            $lookup: {
+              from: "crm_agents",
+              let: { assign_to_agentString: "$assign_to_agent" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ["$_id", { $toObjectId: "$$assign_to_agentString" }],
+                    },
+                  },
+                },
+                {
+                  $project: {
+                    agent_name: 1,
+                  },
+                },
+              ],
+              as: "comment_by",
+            },
+          },
+       ]); 
+       
+       res.status(201).json({  
+        success: true,
+        followuplead, 
+      }); 
+
+       }
+       
+             
+})
 
 
 
--- routes/followupRoute.js
+++ routes/followupRoute.js
@@ -1,8 +1,9 @@
 const express=require('express');
-const { Add_Followup_Lead} = require('../controllers/followupController');
+const { Add_Followup_Lead, getFollowupById } = require('../controllers/followupController');
 
 
 const router=express.Router();
 router.route("/add_followup_lead").post(Add_Followup_Lead);
+router.route("/all_followup_lead_by_id/:id").post(getFollowupById);
 
 module.exports=router;     
