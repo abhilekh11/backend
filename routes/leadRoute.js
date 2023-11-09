8aa1352c4a178946bf68efc01e208ddfd25854a8
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-11-09 17:08:01 +0530

    ss

--- routes/leadRoute.js
+++ routes/leadRoute.js
@@ -8,6 +8,6 @@ router.route("/get_all_lead").get(getAllLead);
 
 router.route("/get_lead_by_id/:id").get(getLeadById); 
 router.route("/delete_all_lead").delete(deleteAllLead);
-router.route("/get_Leadby_agentid_status").get(getLeadbyagentidandstatus)
+router.route("/get_Leadby_agentid_status").post(getLeadbyagentidandstatus)
 
 module.exports=router;     
