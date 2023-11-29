f56b4bb5122f18f73e144a7694fcd64762f67c1a
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-11-29 13:18:52 +0530

    sfdsgdg

--- controllers/genralApiController.js
+++ controllers/genralApiController.js
@@ -71,29 +71,257 @@ const formattedThirtyDaysAgoDate = ThirtyDaysAgoDate.toISOString();
 
 exports.LeadSourceOverviewApi=catchAsyncErrors(async(req,res,next)=>{
        const Lead_source_id=[];
+       const Lead_source_name=[];
 
        const Lead_source_count=[];
        const lead_source=await leadsourceModel.find();
     
-       lead_source.map((lead_source1)=>{
+       lead_source.forEach((lead_source1)=>{
         Lead_source_id.push(lead_source1._id)
+        Lead_source_name.push(lead_source1.lead_source_name)
        })
 
-       Lead_source_id.map((Lead_source_id1)=>{
-       
-             const lead= Lead.find({lead_source:Lead_source_id1});
-            if(!lead){
+       await Promise.all(
+        Lead_source_id.map(async (Lead_source_id1) => {
+            const lead = await Lead.find({ lead_source: Lead_source_id1 });
+            const lead_length = lead.length;
+          
+            if (!lead) {
                 Lead_source_count.push(0);
-              }else{
-                Lead_source_count.push(lead.length);
+            } else {
+                Lead_source_count.push(lead_length);
+            }
+        })
+    );
 
-              }
-             
-       })
-       res.status(201).json({
+    res.status(201).json({
         success: true,
         message: "Successfully Leads Source Overview",
         Lead_source_count,
-      });
+        Lead_source_name,
+    });
 
 });
+
+
+//// Income Graph Overview 
+
+exports.IncomeGraphOverview=catchAsyncErrors(async (req,res,next)=>{
+   
+  const wonstatu= await lead_status.find({status_name:"Won"});
+  const wonStatus_id=wonstatu['0']._id;
+  const monthlyIncom=[];
+  
+     
+////// for jan
+    let total1=0;
+      const lead1= await Lead.find({ 
+        status:wonStatus_id,
+        $expr: {
+          $eq: [
+            { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+            1 // November
+          ]
+        }
+       })
+       lead1.map( (lead11)=>{
+        total1 +=parseInt(lead11.lead_cost);  
+       })
+       monthlyIncom.push(total1);
+ //// for fav
+ let total12=0;
+ const lead2= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       2 // November
+     ]
+   }
+  })
+  lead2.map( (lead22)=>{
+    total12 +=parseInt(lead22.lead_cost);  
+  })
+  monthlyIncom.push(total12);
+
+  /// mar
+ let total13=0;
+ const lead3= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       3 
+     ]
+   }
+  })
+  lead3.map( (lead33)=>{
+    total13 +=parseInt(lead33.lead_cost);  
+  })
+  monthlyIncom.push(total13);
+
+
+   /// apirl 
+ let total14=0;
+ const lead4= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       4 // November
+     ]
+   }
+  })
+  lead4.map( (lead44)=>{
+    total14 +=parseInt(lead44.lead_cost);  
+  })
+  monthlyIncom.push(total14);
+
+  ////// may
+
+  let total15=0;
+ const lead5= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       5 // November
+     ]
+   }
+  })
+  lead5.map( (lead55)=>{
+    total15 +=parseInt(lead55.lead_cost);  
+  })
+  monthlyIncom.push(total15);
+  // june
+
+  let total16=0;
+ const lead6= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       6 // November
+     ]
+   }
+  })
+  lead6.map( (lead66)=>{
+    total16 +=parseInt(lead66.lead_cost);  
+  })
+  monthlyIncom.push(total16);
+
+  //// july
+  let total17=0;
+ const lead7= await Lead.find({ 
+   status:wonStatus_id,
+   $expr: {
+     $eq: [
+       { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+       7 // November
+     ]
+   }
+  })
+  lead7.map( (lead77)=>{
+    total17 +=parseInt(lead77.lead_cost);  
+  })
+  monthlyIncom.push(total17);
+
+  // august
+  let total18=0;
+  const lead8= await Lead.find({ 
+    status:wonStatus_id,
+    $expr: {
+      $eq: [
+        { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+        8 // November
+      ]
+    }
+   })
+   lead8.map( (lead88)=>{
+    total18 +=parseInt(lead88.lead_cost);  
+   })
+   monthlyIncom.push(total18);
+   /// setember
+
+  let total19=0;
+  const lead9= await Lead.find({ 
+    status:wonStatus_id,
+    $expr: {
+      $eq: [
+        { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+        9 // November
+      ]
+    }
+   })
+   lead9.map( (lead99)=>{
+    total19 +=parseInt(lead99.lead_cost);  
+   })
+   monthlyIncom.push(total19);
+   //octuber
+   let total110=0;
+  const lead10= await Lead.find({ 
+    status:wonStatus_id,
+    $expr: {
+      $eq: [
+        { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+        10 // November
+      ]
+    }
+   })
+   lead10.map( (lead1010)=>{
+    total110 +=parseInt(lead1010.lead_cost);  
+   })
+   monthlyIncom.push(total110);
+   /// nomber
+   let total111=0;
+   const lead111= await Lead.find({ 
+     status:wonStatus_id,
+     $expr: {
+       $eq: [
+         { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+         11 // November
+       ]
+     }
+    })
+    lead111.map( (lead1111)=>{
+      total111 +=parseInt(lead1111.lead_cost);  
+    })
+    monthlyIncom.push(total111);
+    /// december
+
+    let total1112=0;
+   const lead1112= await Lead.find({ 
+     status:wonStatus_id,
+     $expr: {
+       $eq: [
+         { $month: '$followup_date' }, // Replace 'yourDateField' with the actual field name
+         12 // November
+       ]
+     }
+    })
+    lead1112.map( (lead11112)=>{
+      total1112 +=parseInt(lead11112.lead_cost);  
+    })
+    monthlyIncom.push(total1112);
+
+   
+ 
+ 
+
+
+
+       res.status(201).json({
+        success: true,
+        message: "Successfully Leads Source Overview",
+        monthlyIncom,
+    });
+  
+          
+
+
+ 
+
+
+  
+})
+
--- routes/genralapiRoute.js
+++ routes/genralapiRoute.js
@@ -1,10 +1,11 @@
 const express=require('express');
-const { YearlySaleApi, LeadSourceOverviewApi  } = require('../controllers/genralApiController');
+const { YearlySaleApi, LeadSourceOverviewApi ,IncomeGraphOverview } = require('../controllers/genralApiController');
 
 
 const router=express.Router();
 
 router.route("/YearlySaleApi").get(YearlySaleApi);
 router.route("/lead_source_overview_api").get(LeadSourceOverviewApi);
+router.route("/Income_Graph_Overview").get(IncomeGraphOverview);  
 
 module.exports=router;     
