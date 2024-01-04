9ce31d981382280835d697eb7a0db6d92375561b
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2024-01-04 16:54:58 +0530

    dcdc

--- routes/leadFileRoute.js
+++ routes/leadFileRoute.js
@@ -7,7 +7,7 @@ const destinationPath = path.join(__dirname, '../public', 'File');
 if (!fs.existsSync(destinationPath)) {
   fs.mkdirSync(destinationPath, { recursive: true });
 }
-excel.use(express.static(path.resolve(__dirname,'../public')));
+excel.use('../public',express.static(path.resolve(__dirname,'../public')));
 var storage= multer.diskStorage({
     destination:(req,file,cb)=>{
        cb(null, destinationPath);  
