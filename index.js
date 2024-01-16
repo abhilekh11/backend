ec11cb3ca9694190dd1d553da2a9eebfc37cf4ec
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2024-01-16 12:39:48 +0530

    ngncbn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ process.on('uncaughtException', (err) => {
 });
 
 connectDatabase();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is working on http://localhost:${PORT}`);
 });
 
 // Unhandled promise rejection
