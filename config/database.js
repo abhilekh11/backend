d626a2a92acef431681d22e7fbe39539203a06b5
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2024-01-16 12:28:26 +0530

    dgvfd

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 
 dotenv.config({path:"./config/config.env"});
+const url='mongodb+srv://main_crm:G23LYJMUGDGCIKEs@cluster0.e2eklhp.mongodb.net/maincrm?retryWrites=true&w=majority';
 const connectDatabase =()=>{
    
-    mongoose.connect(process.env.DB_URI1,{
+    mongoose.connect(url,{
      useNewUrlParser:true,   
      useUnifiedTopology:true,  
    }).then((data)=>{
